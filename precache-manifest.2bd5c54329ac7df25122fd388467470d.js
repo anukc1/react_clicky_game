734578f28524b870da002f04c66d06369211dcc1
Anu KC <anukc2000@gmail.com>
2019-08-08 13:17:07 -0500

    Updates

---
+++ precache-manifest.2bd5c54329ac7df25122fd388467470d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d577121ff965e046683ea64244740f2e",
+    "url": "/react_clicky_game/index.html"
+  },
+  {
+    "revision": "599b383db47e5f9bb2eb",
+    "url": "/react_clicky_game/static/css/main.383b2cc4.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/react_clicky_game/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "599b383db47e5f9bb2eb",
+    "url": "/react_clicky_game/static/js/main.da8d67ae.chunk.js"
+  },
+  {
+    "revision": "da1df628f250dadb7e8d",
+    "url": "/react_clicky_game/static/js/runtime~main.78fa7db4.js"
+  }
+]);
